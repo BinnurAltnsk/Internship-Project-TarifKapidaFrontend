04cfb288408317842066bcaa98561a35c8d2df62
BinnurAltnsk <binnur.altinisik.tr@gmail.com>
2025-08-06 17:36:49 +0300

    Backend deployment and database integration with Railway and PostgreSQL.

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:7175';
+const API_BASE_URL = 'http://localhost:5043';
 localStorage.setItem('API_BASE_URL', API_BASE_URL);
 
 // Axios instance oluştur
